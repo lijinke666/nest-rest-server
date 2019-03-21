50ae179b1827b085dab91a44b18245ebaa624abf
Jinke.Li <1359518268@qq.com>
2019-03-21 17:26:30 +0800

    feat: 新增 cookie & session parser

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { registerSwagger } from './swagger';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: false,
+  });
+
+  app.use(cookieParser());
+
+  app.use(
+    session({
+      secret: 'nest',
+      name: 'nest',
+      cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 设置 cookie 7天后过期
+      resave: true,
+      rolling: true,
+      saveUninitialized: false,
+    }),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
+
   app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 分钟
     max: 100, // 最大 100个 ip
   }));
+
   registerSwagger(app)();
+
   await app.listen(3000);
 }
 bootstrap();
