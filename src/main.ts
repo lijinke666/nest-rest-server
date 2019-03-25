09867fad150194ea2e51c0c953ee255927489a12
Jinke.Li <1359518268@qq.com>
2019-03-25 13:24:10 +0800

    feat: 新增 安全相关中间键

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { registerSwagger } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
 import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
     logger: false,
   });
 
+  // Cookie
   app.use(cookieParser());
 
+  // Session
   app.use(
     session({
       secret: 'nest',
@@ -25,15 +29,25 @@ async function bootstrap() {
     }),
   );
 
+  // 全局参数验证
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 分钟
-    max: 100, // 最大 100个 ip
-  }));
+  // Header 头 安全
+  app.use(helmet());
 
+  // 跨站请求伪造
+  app.use(csurf());
+
+  // 接口访问限制
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 分钟
+      max: 100, // 最大 100个 ip
+    }),
+  );
+
+  // Swagger
   registerSwagger(app)();
 
   await app.listen(3000);
-}
-bootstrap();
+})();
